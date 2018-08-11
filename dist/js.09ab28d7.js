fbee3052e002c79ad60105059837f74aef5b3fb3
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-08-11 23:32:19 +0100

    Added items to menu and transitions. Changed logo.

--- dist/js.09ab28d7.js
+++ dist/js.09ab28d7.js
@@ -173,6 +173,13 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("../scss/main.scss");
+
+var menuIcon = document.querySelector(".fa-bars");
+var body = document.querySelector("body");
+menuIcon.addEventListener("click", function () {
+  body.addClassItem("menu-active");
+  console.log("hi");
+});
 },{"../scss/main.scss":"scss\\main.scss"}],"..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import "../scss/main.scss";
+
+const menuIcon = document.querySelector(".fa-bars");
+const body = document.querySelector("body");
+menuIcon.addEventListener("click", () => {
+  body.addClassItem("menu-active");
+  console.log("hi");
+});
